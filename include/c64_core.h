1d4fc2a1fd4159764f75db5f93f69f22599a4d22
Kernel Panic <kernel_panic_x@protonmail.com>
2025-10-14 00:16:50 +0200

    second commit

---
+++ include/c64_core.h
@@ -0,0 +1,15 @@
+#ifndef C64_CORE_H
+#define C64_CORE_H
+
+#include "c64_types.h"
+
+typedef struct C64_Core C64_Core;
+
+C64_Core *c64_core_create(void);
+void c64_core_destroy(C64_Core *core);
+bool c64_core_step(C64_Core *core);
+void c64_core_reset(C64_Core *core);
+
+C64_Framebuffer *get_c64_framebuffer(C64_Core *core);
+
+#endif // C64_CORE_H
